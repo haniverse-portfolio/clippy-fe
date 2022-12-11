310500b708deee2c1cd793f3d5c3ec0f959dfc05
hyeonwoojo <h_jo@ejn.gg>
2022-12-11 14:23:35 +0900

    feat: aside 개발

--- components/common/MainLayout.tsx
+++ components/common/MainLayout.tsx
@@ -1,16 +1,12 @@
 import { Flex } from "@mantine/core";
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactFragment,
-  ReactPortal,
-} from "react";
 
 // Get Content Component and Aside Component
 const MainLayout = ({ content, aside }: any): JSX.Element => {
   return (
     <Flex justify="space-between">
-      <div style={{ height: "calc(100vh - 120px)", flex: 1 }}>{content()}</div>
+      <div style={{ height: "calc(100vh - 120px)", flex: 1, overflow: "auto" }}>
+        {content()}
+      </div>
       <div className="w-[360px]" style={{ height: "calc(100vh - 120px)" }}>
         {aside()}
       </div>
