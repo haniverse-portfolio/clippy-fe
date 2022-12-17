3e81bc55b50dd1de333e2605b0ca340208a6603b
skymins04 <skymin0417@gmail.com>
2022-12-17 22:56:06 +0900

    feat: 한줄공지 컴포넌트 추가

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -40,6 +40,7 @@ import { CreateModal } from "./CreateModal";
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { TopNotice } from "./TopNotice";
 
 const goLogin = () => {
   // use authorization code grant flow
@@ -132,125 +133,128 @@ export function Navbar() {
       });
   };
   return (
-    <div
-      className="h-[120px] bg-white sticky top-0 z-50"
-      style={{ borderBottom: "1px solid rgba(0, 0, 0, 0.1)" }}
-    >
-      <CreateModal />
-      <Group position="apart">
-        <Flex
-          w={`calc(100% - ${searchBarHidden ? "300px" : "200px"})`}
-          my={36}
-          ml={30}
-          align="center"
-        >
-          <Link href="/" className="lg:mr-[125px] md:mr-[50px] sm:mr-[50px]">
-            <Logo />
-          </Link>
-          {isLogined && (!searchBarHidden || searchBarOpen) && (
-            <TextInput
-              className="max-w-[588px] animate-fadeIn"
-              h={48}
-              size="lg"
-              radius="xl"
-              id="navbar-title"
-              placeholder="닉네임, 제목 키워드"
-              value={searchText}
-              onChange={(e) => {
-                let changedText = e.currentTarget.value as string;
-                let textLength = changedText.length;
-                // if (textLength === 0) {
-                //   setSearchText(changedText);
-                //   return;
-                // }
-                // let lastWord = changedText[textLength - 1];
-                // let ac = lastWord.charCodeAt(0); // ascii_code
-                // let validFlag =
-                //   (ac >= 48 && ac <= 57) ||
-                //   (ac >= 65 && ac <= 90) ||
-                //   (ac >= 97 && ac <= 122) ||
-                //   ac === 95;
-                // if (validFlag) setSearchText(changedText);
-                setSearchText(changedText);
-              }}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  if (searchText === "") return;
-                  getTwitchChannel(searchText);
-                  router.push(`/search/${searchText}`);
-                }
-              }}
-              icon={
-                <FontAwesomeIcon
-                  id="navbar-search-icon"
-                  icon={solid("magnifying-glass")}
-                  className="w-5 ml-[30px]"
-                  fontWeight={900}
-                  color="#DEDEDE"
-                />
-              }
-              style={{
-                width: searchBarHidden ? "calc(100% - 200px)" : "100%",
-                left: searchBarHidden ? "30px" : "auto",
-                animation: "fadeIn .4s ease-in-out",
-                position: searchBarHidden ? "absolute" : "relative",
-                boxShadow: "0px 4px 15px rgba(119, 119, 119, 0.25)",
-                borderRadius: "99px",
-                border: 0,
-              }}
-              rightSection={
-                searchText.length > 0 && (
-                  <FontAwesomeIcon
-                    className="w-5 mr-[30px] cursor-pointer"
-                    icon={regular("circle-xmark")}
-                    onClick={() => {
-                      setSearchText("");
-                    }}
-                  />
-                )
-              }
-            />
-          )}
-        </Flex>
-
-        {isLogined && (
-          <Flex justify="flex-end" align="center">
-            {searchBarHidden && (
-              <div
-                onClick={() => setSearchBarOpen(!searchBarOpen)}
-                className="w-[44px] h-[44px] mr-[10px] rounded-full bg-gray-100 flex justify-center align-middle hover:cursor-pointer"
-              >
-                <FontAwesomeIcon
-                  icon={
-                    searchBarOpen ? solid("check") : solid("magnifying-glass")
+    <>
+      <TopNotice />
+      <div
+        className="h-[120px] bg-white sticky top-0 z-50"
+        style={{ borderBottom: "1px solid rgba(0, 0, 0, 0.1)" }}
+      >
+        <CreateModal />
+        <Group position="apart">
+          <Flex
+            w={`calc(100% - ${searchBarHidden ? "300px" : "200px"})`}
+            my={36}
+            ml={30}
+            align="center"
+          >
+            <Link href="/" className="lg:mr-[125px] md:mr-[50px] sm:mr-[50px]">
+              <Logo />
+            </Link>
+            {isLogined && (!searchBarHidden || searchBarOpen) && (
+              <TextInput
+                className="max-w-[588px] animate-fadeIn"
+                h={48}
+                size="lg"
+                radius="xl"
+                id="navbar-title"
+                placeholder="닉네임, 제목 키워드"
+                value={searchText}
+                onChange={(e) => {
+                  let changedText = e.currentTarget.value as string;
+                  let textLength = changedText.length;
+                  // if (textLength === 0) {
+                  //   setSearchText(changedText);
+                  //   return;
+                  // }
+                  // let lastWord = changedText[textLength - 1];
+                  // let ac = lastWord.charCodeAt(0); // ascii_code
+                  // let validFlag =
+                  //   (ac >= 48 && ac <= 57) ||
+                  //   (ac >= 65 && ac <= 90) ||
+                  //   (ac >= 97 && ac <= 122) ||
+                  //   ac === 95;
+                  // if (validFlag) setSearchText(changedText);
+                  setSearchText(changedText);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    if (searchText === "") return;
+                    getTwitchChannel(searchText);
+                    router.push(`/search/${searchText}`);
                   }
-                  width="50%"
-                  className="text-gray-400"
-                />
-              </div>
+                }}
+                icon={
+                  <FontAwesomeIcon
+                    id="navbar-search-icon"
+                    icon={solid("magnifying-glass")}
+                    className="w-5 ml-[30px]"
+                    fontWeight={900}
+                    color="#DEDEDE"
+                  />
+                }
+                style={{
+                  width: searchBarHidden ? "calc(100% - 200px)" : "100%",
+                  left: searchBarHidden ? "30px" : "auto",
+                  animation: "fadeIn .4s ease-in-out",
+                  position: searchBarHidden ? "absolute" : "relative",
+                  boxShadow: "0px 4px 15px rgba(119, 119, 119, 0.25)",
+                  borderRadius: "99px",
+                  border: 0,
+                }}
+                rightSection={
+                  searchText.length > 0 && (
+                    <FontAwesomeIcon
+                      className="w-5 mr-[30px] cursor-pointer"
+                      icon={regular("circle-xmark")}
+                      onClick={() => {
+                        setSearchText("");
+                      }}
+                    />
+                  )
+                }
+              />
             )}
-            <Avatar
-              onClick={() => {
-                setDrawerOpened(true);
-              }}
-              radius="xl"
-              size="lg"
-              w={48}
-              h={48}
-              mr={30}
-              id="navbar-avatar"
-              src={loginUserInfo.profileImageUrl}
-            />
           </Flex>
-        )}
-      </Group>
-      {/* <Stack className="h-[60px] bg-[#63C81B]">
+
+          {isLogined && (
+            <Flex justify="flex-end" align="center">
+              {searchBarHidden && (
+                <div
+                  onClick={() => setSearchBarOpen(!searchBarOpen)}
+                  className="w-[44px] h-[44px] mr-[10px] rounded-full bg-gray-100 flex justify-center align-middle hover:cursor-pointer"
+                >
+                  <FontAwesomeIcon
+                    icon={
+                      searchBarOpen ? solid("check") : solid("magnifying-glass")
+                    }
+                    width="50%"
+                    className="text-gray-400"
+                  />
+                </div>
+              )}
+              <Avatar
+                onClick={() => {
+                  setDrawerOpened(true);
+                }}
+                radius="xl"
+                size="lg"
+                w={48}
+                h={48}
+                mr={30}
+                id="navbar-avatar"
+                src={loginUserInfo.profileImageUrl}
+              />
+            </Flex>
+          )}
+        </Group>
+        {/* <Stack className="h-[60px] bg-[#63C81B]">
         <span className="ml-8 my-auto text-xl text-white font-semibold">
           [공지] 12월 14일 오후 6시까지{" "}
           <span className="underline">클립 생성</span> 기능이 일시적으로
           제한됩니다.
         </span>
       </Stack> */}
-    </div>
+      </div>
+    </>
   );
 }
